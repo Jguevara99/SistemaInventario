a44bc19a9a40dcd2d5a20039c873fcaf723561c2
Jairo Guevara <guevarajairo294@gmail.com>
2024-06-22 11:48:04 -0600

    1 - Implementacion de BD y Arquitectura a utilizar(Estructura de carpetas), 2 - Instalar paquetes Nuget, 3 - Organizar el Context.

---
+++ POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using POS.Infrastructure.Persistences.Contexts;
+
+namespace POS.Infrastructure.Extensions;
+
+public static class InjectionExtensions
+{
+    public static IServiceCollection AddInjectionInfraestructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var assembly = typeof(SISVENTASContext).Assembly.FullName;
+
+        services.AddDbContext<SISVENTASContext>(
+            options => options.UseSqlServer(
+                configuration.GetConnectionString("POSTConnection"), b => b.MigrationsAssembly(assembly)
+                ),
+            ServiceLifetime.Transient
+            );
+
+        return services;
+
+    }
+}
